524b24da791be8189eb5b29aed74702931199fda
phischolz <33486489+phischolz@users.noreply.github.com>
2021-05-09 13:23:18 +0200

    Formatting

--- src/components/Processes.js
+++ src/components/Processes.js
@@ -11,63 +11,63 @@ const EnzianYellow = require("enzian-yellow");
 
 class Processes extends Component{
 
-enzian;
+  enzian;
 
-    state = {
-      selectedContract: "Select a contract...",
-      storedContracts: [],
-      storedConnections: [],
-      currentEventLog: [],
-      taskToBeExecuted: -1,
-      selectedConnection: "Select Connection...",
-      connectionSelected: false,
-      newContractAddress: '',
-      waitForVerification: false
+  state = {
+    selectedContract: "Select a contract...",
+    storedContracts: [],
+    storedConnections: [],
+    currentEventLog: [],
+    taskToBeExecuted: -1,
+    selectedConnection: "Select Connection...",
+    connectionSelected: false,
+    newContractAddress: '',
+    waitForVerification: false
+  }
+
+  loadContracts = () => {
+    let contracts = JSON.parse(localStorage.getItem('contracts'));
+    if(!contracts) {
+      contracts = [];
+    }
+    this.setState({ storedContracts: contracts });
+  }
+
+  componentDidMount() {
+
+    this.loadContracts();
+
+    let web3Connections = JSON.parse(localStorage.getItem("web3Connections"));
+    if(!web3Connections) {
+      web3Connections = [];
     }
 
-    loadContracts = () => {
-      let contracts = JSON.parse(localStorage.getItem('contracts'));
-      if(!contracts) {
-        contracts = new Array();
-      }
-      this.setState({ storedContracts: contracts });
+    if (window.ethereum) {
+      web3Connections.push("MetaMask");
     }
 
-    componentDidMount() {
-     
-      this.loadContracts();
 
-      let web3Connections = JSON.parse(localStorage.getItem("web3Connections"));
-      if(!web3Connections) {
-        web3Connections = new Array();
-      }
- 
-      if (window.ethereum) {
-        web3Connections.push("MetaMask");
-      }
+    this.setState({ storedConnections: web3Connections });
 
+  }
 
-      this.setState({ storedConnections: web3Connections });
+  handleValueChange = (valueAsNumber, valueAsString) => {
+    this.setState({ taskToBeExecuted: valueAsNumber });
+  };
 
+  renderContractAddress = (storedContract, { handleClick, modifiers }) => {
+    if (modifiers && !modifiers.matchesPredicate) {
+        return null;
     }
 
-    handleValueChange = (valueAsNumber, valueAsString) => {
-      this.setState({ taskToBeExecuted: valueAsNumber });
-    };
-
-    renderContractAddress = (storedContract, { handleClick, modifiers }) => {
-      if (modifiers && !modifiers.matchesPredicate) {
-          return null;
-      }
-
-      return (
-          <MenuItem
-              active={modifiers.active}
-              key={storedContract}
-              onClick={handleClick}
-              text={storedContract}
-          />
-      );
+    return (
+        <MenuItem
+            active={modifiers.active}
+            key={storedContract}
+            onClick={handleClick}
+            text={storedContract}
+        />
+    );
   };
 
 
@@ -94,26 +94,28 @@ enzian;
   onExecuteClick = async () => {
 
     this.setState({waitForVerification: true})
+    let result;
 
     switch(this.state.selectedConnection) {
+
       case 'MetaMask':
       
         this.enzian = new EnzianYellow(window.ethereum);
-        let theresult = await this.enzian.executeTaskByAddress(
+        result = await this.enzian.executeTaskByAddress(
           this.state.selectedContract,
           this.state.taskToBeExecuted
         );
-        console.log(theresult);
+        console.log(result);
       
       break;
       default:
         this.enzian = new EnzianYellow(new Web3(new Web3.providers.HttpProvider(this.state.selectedConnection)));
-        theresult = await this.enzian.executeTaskBySelfSigned(
+        result = await this.enzian.executeTaskByAddress(
           this.state.selectedContract,
           this.state.taskToBeExecuted,
           localStorage.getItem('selectedPrivateKey')
         );
-        console.log(theresult);
+        console.log(result);
         break;
     }
 
@@ -135,9 +137,7 @@ enzian;
 
   addNewContractAddress = () => {
     let contracts = JSON.parse(localStorage.getItem("contracts"));
-    if(!contracts) {
-      contracts = new Array();
-    }
+    if(!contracts) contracts = [];
     contracts.push(this.state.newContractAddress);
     localStorage.setItem("contracts", JSON.stringify(contracts));
     this.loadContracts();
@@ -150,91 +150,97 @@ enzian;
   }
 
 
-    render(){
-      return(
-          <div>
-              <Header setAndUpdateConnection={this.setAndUpdateConnection} />
-              <div className="content">
-                  <h1>Deployed Processes on the current Blockchain</h1>
-                  <div >
+  render(){
+    return(
+        <div>
+            <Header setAndUpdateConnection={this.setAndUpdateConnection} />
+            <div className="content">
+                <h1>Deployed Processes on the current Blockchain</h1>
+                <div >
 
-                      <div style={{margin: '10px', display: 'flex'}}>
-                        <div style={{margin: '10px'}}>
-                        <h5>Select a deployed Contract</h5>
+                    <div style={{margin: '10px', display: 'flex'}}>
+                      <div style={{margin: '10px'}}>
+                      <h5>Select a deployed Contract</h5>
 
-                        <Select 
-                            items={this.state.storedContracts}
-                            itemRenderer={this.renderContractAddress}
-                            noResults={<MenuItem disabled={true} text="No results." />}
-                            onItemSelect={this.contractAddressSelected}
-                        >
-                            {/* children become the popover target; render value here */}
-                            <Button text={this.state.selectedContract} rightIcon="double-caret-vertical" />
-                        </Select>
+                      <Select
+                          items={this.state.storedContracts}
+                          itemRenderer={this.renderContractAddress}
+                          noResults={<MenuItem disabled={true} text="No results." />}
+                          onItemSelect={this.contractAddressSelected}
+                      >
+                          {/* children become the popover target; render value here */}
+                          <Button text={this.state.selectedContract} rightIcon="double-caret-vertical" />
+                      </Select>
 
-                        </div>
-                        <div style={{margin: '10px'}}>
-                        <h5>or Add a new Contract Address</h5>
+                      </div>
+                      <div style={{margin: '10px'}}>
+                      <h5>or Add a new Contract Address</h5>
+
+                      <ControlGroup>
+                        <InputGroup onChange={this.updateNewContractAddress} id="text-input"
+                                    placeholder="Paste the Contract Address here..."
+                                    intent="primary"
+                                    style={{
+                                      width: '400px',
+                                      fontFamily: 'Consolas,Monaco,Lucida Console,Liberation Mono,DejaVu Sans Mono,' +
+                                          'Bitstream Vera Sans Mono,Courier New, monospace'}} />
+                        <Button
+                              intent="primary"
+                              rightIcon="floppy-disk"
+                              onClick={this.addNewContractAddress}
+                          />
+                      </ControlGroup>
+
+                      </div>
+                    </div>
+
+                    <div >
+                      <div style={{margin: '10px'}}>
+                        <h5>Event Log:</h5>
+                        {
+                            this.state.currentEventLog.map(task => {
+                              return (  <Tag style={{margin: '5px'}} minimal="true" intent="success" large="true" key={task} > {task}</Tag> )
+                          })
+                        }
+                      </div>
+                    </div>
+
+                    <div >
+                      <div style={{margin: '10px'}}>
+                      <h5>Execute Task</h5>
 
                         <ControlGroup>
-                          <InputGroup onChange={this.updateNewContractAddress} id="text-input" placeholder="Paste the Contract Address here..."  intent="primary" style={{width: '400px', fontFamily: 'Consolas,Monaco,Lucida Console,Liberation Mono,DejaVu Sans Mono,Bitstream Vera Sans Mono,Courier New, monospace'}} />
-                          <Button
-                                intent="primary"
-                                rightIcon="floppy-disk"
-                                onClick={this.addNewContractAddress}
-                            />
-                          </ControlGroup>
+                         {
+                           this.state.waitForVerification ?
+                           <div>
+                             <Spinner size={Spinner.SIZE_SMALL}/>
+                           </div>
+                           :
+                           <div>
 
-                        </div>
+                             <NumericInput onValueChange={this.handleValueChange} />
+                        <br />
+                        <Button
+                            intent="primary"
+                            text='Execute'
+                            rightIcon="flow-linear"
+                            onClick={this.onExecuteClick}
+                        />
+                           </div>
+
+                         }
+
+                        </ControlGroup>
+
+
                       </div>
+                    </div>
 
-                      <div >
-                        <div style={{margin: '10px'}}>
-                          <h5>Event Log:</h5>
-                          {
-                              this.state.currentEventLog.map(task => {
-                                return (  <Tag style={{margin: '5px'}} minimal="true" intent="success" large="true" key={task} > {task}</Tag> )
-                            })
-                          }
-                        </div>
-                      </div>
-
-                      <div >
-                        <div style={{margin: '10px'}}>
-                        <h5>Execute Task</h5>
-                          
-                          <ControlGroup>
-                           {
-                             this.state.waitForVerification ?
-                             <div>
-                               <Spinner size={Spinner.SIZE_SMALL}/>
-                             </div>
-                             : 
-                             <div>
-
-                               <NumericInput onValueChange={this.handleValueChange} />
-                          <br />
-                          <Button
-                              intent="primary"
-                              text='Execute'
-                              rightIcon="flow-linear"
-                              onClick={this.onExecuteClick}
-                          />
-                             </div>
-                            
-                           }
-                          
-                          </ControlGroup>
-                         
-
-                        </div>
-                      </div>
-
-                  </div>
-              </div>
-          </div>
-      );
-    }
+                </div>
+            </div>
+        </div>
+    );
   }
-  
-  export default hot(module)(Processes);
+}
+
+export default hot(module)(Processes);
