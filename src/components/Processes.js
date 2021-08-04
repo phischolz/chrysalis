c089772c9c2d5e6b8d40e99da570d2ff29d1524f
phischolz <33486489+phischolz@users.noreply.github.com>
2021-08-04 12:57:32 +0200

    added web3 dependency for metamask support, added ABI selector again.

--- src/components/Processes.js
+++ src/components/Processes.js
@@ -3,10 +3,17 @@ import {hot} from "react-hot-loader";
 
 import Header from './Header';
 
-import {MenuItem, Button, NumericInput, ControlGroup, InputGroup, Tag, Spinner} from "@blueprintjs/core";
+import {
+    MenuItem,
+    Button,
+    ControlGroup,
+    Tag,
+    Spinner,
+    RadioGroup,
+    Radio, FileInput
+} from "@blueprintjs/core";
 import {Select} from "@blueprintjs/select";
 
-const Web3 = require("web3");
 import restConfig from "../dataExchange/connection/rest-config.json"
 import ExchangeHandler from "../dataExchange/connection/ExchangeHandler";
 
@@ -28,7 +35,14 @@ class Processes extends Component {
         connectionSelected: false,
         newContractAddress: '',
         newContractName: '',
-        waitForVerification: false
+        waitForVerification: false,
+
+        selectedAbi: '',
+        storedAbis: [],
+        selectedStoredAbiId: 'custom',
+        selectedFile: undefined,
+        chainTypes: ['ethereum', 'hyperledger'],
+        selectedChainType: 'ethereum',
     }
 
     SERVER_ENDPOINT = restConfig.SERVER_URL + '/processes'
@@ -44,6 +58,9 @@ class Processes extends Component {
 
     componentDidMount() {
         this.fetchContracts();
+        ExchangeHandler.sendRequest('GET', restConfig.SERVER_URL + '/abis').then(response => {
+            this.setState({storedAbis: response.data})
+        })
     }
 
     renderContractAddress = (storedContract, {handleClick, modifiers}) => {
@@ -61,12 +78,16 @@ class Processes extends Component {
         );
     };
 
+    selectedStoredAbiChanged = (event) => {
+        this.setState({selectedStoredAbiId: event.target.value})
+    };
 
     contractAddressSelected = (e) => {
         console.log(e)
         this.setState({selectedContract: e, selectedContractTasks: e.tasks});
-        this.initEnzian();
 
+
+        this.initEnzian();
         this.enzian.eventLog(e.address).then(r => {
             this.setState({currentEventLog: r})
         });
@@ -74,15 +95,68 @@ class Processes extends Component {
     }
 
     initEnzian = () => {
-        if (!this.enzian) {
-            if (this.state.selectedConnection.address === 'MetaMask') {
+        switch(this.state.selectedChainType){
+            case "ethereum":
+                if (this.state.selectedStoredAbiId === 'custom') {
+                    console.log("Reading abi from file");
+                } else {
+                    let selectedStoredAbi = this.state.storedAbis.find(abi => abi.id.toString() === this.state.selectedStoredAbiId)
+                    this.setState({selectedAbi: JSON.parse(selectedStoredAbi.abi)});
+                    console.log("Reading abi from storage")
+                }
 
-                this.enzian = new EnzianYellow(window.ethereum);
-            } else {
-                this.enzian = new EnzianYellow(this.state.selectedConnection.address, this.state.selectedStoredAccount.privateKey, 'ethereum');
-
-            }
+                if (this.state.selectedConnection.address === 'MetaMask') {
+                    this.enzian = new EnzianYellow(
+                        window.ethereum,
+                        undefined,
+                        'ethereum',
+                        {compiled: this.state.selectedAbi}
+                    );
+                } else {
+                    this.enzian = new EnzianYellow(
+                        this.state.selectedConnection.address,
+                        this.state.selectedStoredAccount.privateKey,
+                        'ethereum',
+                        {compiled: this.state.selectedAbi}
+                    );
+                }
+                break;
+            default:
+                console.log("No known network type selected!")
+                break;
         }
+    }
+
+    readABI = async (e) => {
+        e.preventDefault()
+        const reader = new FileReader()
+        reader.onload = async (e) => {
+            const text = (e.target.result)
+            console.log(JSON.parse(text));
+            this.setState({selectedAbi: JSON.parse(text)})
+        };
+        reader.readAsText(e.target.files[0]);
+        this.setState({selectedFile: e.target.files[0].name});
+    }
+
+    renderChainType = (selectedChainType, {handleClick, modifiers}) => {
+        if (modifiers && !modifiers.matchesPredicate) {
+            return null;
+        }
+        return (
+            <MenuItem
+                active={modifiers.active}
+                key={selectedChainType}
+                onClick={handleClick}
+                text={selectedChainType}
+            />
+        );
+    }
+
+    chainTypeSelected = (e) => {
+        console.log("selected Chain Type: " + e);
+        this.setState({selectedChainType: e}, this.setEnzian);
+
     }
 
     renderTask = (selectedTask, {handleClick, modifiers}) => {
@@ -109,27 +183,12 @@ class Processes extends Component {
         this.setState({waitForVerification: true})
         let result;
 
-        switch (this.state.selectedConnection.address) {
-
-            case 'MetaMask':
-
-                this.enzian = new EnzianYellow(window.ethereum);
-                result = await this.enzian.executeTask(
-                    this.state.selectedContract.address,
-                    this.state.taskToBeExecuted.number
-                );
-                console.log("tx returned:   ", result);
-
-                break;
-            default:
-                this.enzian = new EnzianYellow(this.state.selectedConnection.address, this.state.selectedStoredAccount.privateKey, 'ethereum');
-                result = await this.enzian.executeTask(
-                    this.state.selectedContract.address,
-                    this.state.taskToBeExecuted.number,
-                );
-                console.log("tx returned: ", result);
-                break;
-        }
+        this.initEnzian();
+        result = await this.enzian.executeTask(
+            this.state.selectedContract.address,
+            this.state.taskToBeExecuted.number,
+        );
+        console.log("tx returned: ", result);
 
         this.setState({waitForVerification: false})
 
@@ -185,9 +244,70 @@ class Processes extends Component {
                     <h1>Deployed Processes on the current Blockchain</h1>
                     <div>
 
+                        {/* Network type selector - if network===ethereum, also ABI selector */}
+                        <div>
+                            <div style={{margin: '10px', maxWidth: '500px'}}>
+                                <h5>Network type</h5>
+
+                                <p>Select the network type your contract was deployed in:</p>
+                                <Select
+                                    items={this.state.chainTypes}
+                                    itemRenderer={this.renderChainType}
+                                    noResults={<MenuItem disabled={false} text="No results."/>}
+                                    onItemSelect={this.chainTypeSelected}
+                                >
+                                    {/* children become the popover target; render value here */}
+                                    <Button text={this.state.selectedChainType} rightIcon="double-caret-vertical"/>
+                                </Select>
+
+
+                                <div>
+                                    {this.state.selectedChainType === 'ethereum' ?
+                                        <div>
+
+                                            <div>
+                                                <h5>Select the ethereum contract ABI:</h5>
+
+                                                <RadioGroup
+                                                    onChange={this.selectedStoredAbiChanged}
+                                                    selectedValue={this.state.selectedStoredAbiId}
+                                                >
+                                                    {
+                                                        this.state.storedAbis.map(abi => {
+                                                            return (
+                                                                <Radio key={abi.id.toString()} value={abi.id.toString()}
+                                                                       label={abi.key}/>)
+                                                        })
+                                                    }
+                                                    <Radio key="custom" value="custom" label="Custom"/>
+                                                </RadioGroup>
+
+                                            </div>
+
+                                            {
+                                                this.state.selectedStoredAbiId === 'custom' ?
+                                                    <div style={{margin: '10px'}}>
+                                                        <h5>Upload the Contract-ABI with the linked Decision
+                                                            Library</h5>
+                                                        <FileInput text='Select an ABI...'
+                                                                   onInputChange={this.readABI}/>
+                                                    </div>
+                                                    : ''
+                                            }
+                                        </div>
+                                        : ''
+                                    }
+                                </div>
+                                <span/>
+
+                            </div>
+                        </div>
+
+                        {/* Process selector */}
                         <div style={{margin: '10px', display: 'flex'}}>
+
                             <div style={{margin: '10px'}}>
-                                <h5>Select a deployed Contract</h5>
+                                <h5>Select a deployed Process</h5>
 
                                 <Select
                                     items={this.state.storedContracts}
@@ -198,8 +318,8 @@ class Processes extends Component {
                                     {/* children become the popover target; render value here */}
                                     <Button text={this.state.selectedContract.name} rightIcon="double-caret-vertical"/>
                                 </Select>
-
                             </div>
+
                             <div className="contract">
                                 <h5>or Add a new Contract Address</h5>
                                 <form onSubmit={this.addNewContractAddress}>
